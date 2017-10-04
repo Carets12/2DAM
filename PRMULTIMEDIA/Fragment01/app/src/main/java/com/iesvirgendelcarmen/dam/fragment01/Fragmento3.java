a0b91d606594955dd6d446b20ef4fc5893f45e1b
Carets12 <danielsierraraez@gmail.com>
2017-10-04 13:53:38 +0200

    04/10/17

---
+++ PRMULTIMEDIA/Fragment01/app/src/main/java/com/iesvirgendelcarmen/dam/fragment01/Fragmento3.java
@@ -0,0 +1,19 @@
+package com.iesvirgendelcarmen.dam.fragment01;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by matinal on 4/10/17.
+ */
+
+public class Fragmento3 extends Fragment {
+
+  public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+      return inflater.inflate(R.layout.fragmento3,container,false);
+  }
+}
+
