4ad2193910f202f301c12e43f603c3fe63a49d63
Daniel Sierra Ráez <danielsierraraez@gmail.com>
2017-10-19 10:46:56 +0200

    19/10/17

--- PRMULTIMEDIA/Fragment05/app/src/main/java/es/vcarmen/fragment05/Fragment05.java
+++ PRMULTIMEDIA/Fragment05/app/src/main/java/es/vcarmen/fragment05/Fragment05.java
@@ -5,19 +5,32 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Fragment05 extends AppCompatActivity {
+    FragmentManager FM;
+    FragmentTransaction FT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment05);
-        FragmentManager FM = getFragmentManager();
-        FragmentTransaction FT = FM.beginTransaction();
 
-        Fragmento1 preferencias = new Fragmento1();
+        Button mostrar = (Button) findViewById(R.id.boton);
+        mostrar.setOnClickListener(new View.OnClickListener() {
 
-        FT.replace(android.R.id.content, preferencias);
-        FT.commit();
-        }
+            @Override
+            public void onClick(View view) {
+                FM = getFragmentManager();
+                FT = FM.beginTransaction();
+                Fragmento1 preferencias = new Fragmento1();
+
+                FT.add(R.id.content, preferencias);
+               // FT.replace(android.R.id.content, preferencias);
+                FT.commit();
+            }
+
+        });
+    }
 }
