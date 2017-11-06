37b0a2f1e7a36b352278efe2d3e551dd66161810
Daniel Sierra Ráez <danielsierraraez@gmail.com>
2017-11-06 13:44:09 +0100

    s

---
+++ DSINTERFAZ/Tabs02/app/src/main/java/vcarmen/es/tabs02/Fotografia.java
@@ -0,0 +1,16 @@
+package vcarmen.es.tabs02;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by matinal on 6/11/17.
+ */
+
+    public class Fotografia extends AppCompatActivity {
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fotografia);
+        }
+    }
+}
---
+++ DSINTERFAZ/Tabs02/app/src/main/java/vcarmen/es/tabs02/Videos.java
@@ -0,0 +1,15 @@
+package vcarmen.es.tabs02;
+
+/**
+ * Created by matinal on 6/11/17.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Videos extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videos);
+    }
+}
