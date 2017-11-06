68908d08a7b3df08797fc1a8e600419e6dbbd17c
Daniel Sierra Ráez <danielsierraraez@gmail.com>
2017-11-06 10:10:22 +0100

    Openbravo Practica

---
+++ ACDATOS/NetBeansProjects/HolaServlet/src/java/com/iesvdc/acceso/holaServlet.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iesvdc.acceso;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author matinal
+ */
+//@WebServlet("/holaServlet")
+public class holaServlet extends HttpServlet {
+      
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */                  
+            
+         
+        }
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+         PrintWriter out = response.getWriter();
+
+         out.println("<html>");
+         out.println("<head></head>");         
+         out.println("<body>");
+
+         out.println("Usuario:");
+         String nombre=request.getParameter("name");
+         out.println(nombre);
+         out.println("<br>");
+         out.println("Clave:");         
+         String contrasena=request.getParameter("password");
+         out.println(contrasena);
+         
+         out.println("</body>");
+         out.println("</html>");           
+    }
+
+}
