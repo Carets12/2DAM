5c1304652ae376d65e3116fd2f1ef544b2f5acf3
Daniel Sierra Ráez <danielsierraraez@gmail.com>
2017-11-07 13:45:13 +0100

    s

---
+++ DSINTERFAZ/ChatApp/app/src/main/java/es/vcarmen/chatapp/Mensaje.java
@@ -0,0 +1,29 @@
+package es.vcarmen.chatapp;
+
+/**
+ * Created by matinal on 7/11/17.
+ */
+
+public class Mensaje {
+
+    private String msg;
+
+    public Mensaje(){
+
+    }
+
+    public Mensaje(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+
+}
+
