1c03cdffdeccd5c769d3a2f70a689d325856e021
Daniel Sierra <Danielsierraraez@gmail.com>
2017-11-27 00:02:52 +0100

    27/11

---
+++ PRMULTIMEDIA/Material09Terminar/app/src/main/java/es/vcarmen/material09/Material09.java
@@ -0,0 +1,18 @@
+package es.vcarmen.material09;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Material09 extends AppCompatActivity {
+
+   // private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material09);
+
+       // toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+}
