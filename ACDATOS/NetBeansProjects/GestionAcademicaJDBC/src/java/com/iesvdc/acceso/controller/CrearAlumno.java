0fc9ccf0f7236e6d89dec5a6970395b37c96246b
Daniel Sierra Ráez <danielsierraraez@gmail.com>
2017-11-29 14:09:46 +0100

    29/11

---
+++ ACDATOS/NetBeansProjects/GestionAcademicaJDBC/src/java/com/iesvdc/acceso/controller/CrearAlumno.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iesvdc.acceso.controller;
+
+import com.iesvdc.acceso.dao.AlumnoDAO;
+import com.iesvdc.acceso.dao.AlumnoDAOImpl;
+import com.iesvdc.acceso.dao.DAOException;
+import com.iesvdc.acceso.pojo.Alumno;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author profesor
+ */
+public class CrearAlumno extends HttpServlet {
+    private Properties props;
+    private Integer driver;
+    private String host;
+    private String puerto;
+    private String base_datos;
+    private String usuario;
+    private String password;
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CrearAlumno</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet CrearAlumno at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // processRequest(request, response);
+        Alumno al;
+        AlumnoDAO al_dao;
+        
+        // Cargar un fichero de texto desde un Servlet
+	/*ServletContext context = getServletContext();
+	InputStream resourceContent = context.getResourceAsStream("/WEB-INF/lib/db.prop");
+        props = new Properties();
+        props.load(resourceContent);
+        this.base_datos = props.getProperty("base_datos");
+        this.driver     = Integer.parseInt(props.getProperty("driver"));
+        this.host       = props.getProperty("host");
+        this.password   = props.getProperty("password");
+        this.usuario    = props.getProperty("usuario");
+        this.puerto     = props.getProperty("puerto");  
+        */
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CrearAlumno</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            al=new Alumno("Juan", "Sin Miedo");
+
+            al_dao=new AlumnoDAOImpl();
+
+            try {
+                al_dao.create(al);
+                out.println("Alumno creado con éxito");
+            } catch (DAOException ex) {
+                out.println(ex.getLocalizedMessage());
+                Logger.getLogger(CrearAlumno.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- PRMULTIMEDIA/Material09/app/src/main/java/es/vcarmen/material09/Material09.java
+++ PRMULTIMEDIA/Material09/app/src/main/java/es/vcarmen/material09/Material09.java
@@ -2,12 +2,98 @@ package es.vcarmen.material09;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Material09 extends AppCompatActivity {
+
+    LinearLayout detalle;
+    ImageView icono;
+    boolean giro;
+    private static final int Duracion = 250;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material09);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle("EL RAPTO DE LAS MALVINAS");
+        toolbar.setSubtitle("Jacques-louis David");
+
+        detalle = (LinearLayout) findViewById(R.id.linear2);
+        giro = false;
+        icono = (ImageView) findViewById(R.id.imagen2);
     }
+
+    private void rotate(float angulo){
+
+        Animation animacion = new RotateAnimation(0.0f,angulo,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        animacion.setFillAfter(true);
+        animacion.setDuration(Duracion);
+        icono.startAnimation(animacion);
+    }
+
+    public void tocado(View view){
+
+        if(giro == true){
+
+            icono.setImageResource(R.mipmap.mas);
+            rotate(-180.0f);
+            giro = false;
+            Dinamizador.contrae(detalle, Duracion);
+        }
+
+        if(giro == false){
+
+            icono.setImageResource(R.mipmap.mas);
+            rotate(180.0f);
+            giro = true;
+            Dinamizador.expande(detalle, Duracion);
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String mensaje = "";
+
+        switch (item.getItemId()) {
+
+            case R.id.opcion1:
+                mensaje = "¡¡¡Opción A Pulsada!!!";
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.opcion2:
+                mensaje = "¡¡¡Opción B Pulsada!!!";
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.opcion3:
+                mensaje = "¡¡¡Opción C Pulsada!!!";
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
